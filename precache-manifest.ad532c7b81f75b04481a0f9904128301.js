8cbf759fae8e5e1f8263ed21a78d8a6049d6a370
b3n3dict <benedict.xavier.e@gmail.com>
2020-04-27 11:29:55 +0530

    Updates

---
+++ precache-manifest.ad532c7b81f75b04481a0f9904128301.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc7da896ffa02f37305047d70966a25e",
+    "url": "/b3n3dict/robofriends/index.html"
+  },
+  {
+    "revision": "a76249b7330f2fa9434c",
+    "url": "/b3n3dict/robofriends/static/css/2.66b7f74f.chunk.css"
+  },
+  {
+    "revision": "2072945231bd948f0be7",
+    "url": "/b3n3dict/robofriends/static/css/main.c05b098e.chunk.css"
+  },
+  {
+    "revision": "a76249b7330f2fa9434c",
+    "url": "/b3n3dict/robofriends/static/js/2.2f7a48c9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/b3n3dict/robofriends/static/js/2.2f7a48c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2072945231bd948f0be7",
+    "url": "/b3n3dict/robofriends/static/js/main.ea712a15.chunk.js"
+  },
+  {
+    "revision": "54e30410e4034cd8fd0f",
+    "url": "/b3n3dict/robofriends/static/js/runtime-main.8083b0f5.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/b3n3dict/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
